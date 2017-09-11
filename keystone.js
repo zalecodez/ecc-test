5f1c660c8e54e457242aeb781ef4d993ad8fb2a8
Zale Young <zale@MacBook-Pro.local>
2017-09-11 12:25:19 -0500

    Modify wysiwyg editor

--- keystone.js
+++ keystone.js
@@ -24,6 +24,18 @@ keystone.init({
 	'auth': true,
 	'user model': 'User',
 
+	'wysiwyg override toolbar': false,
+	'wysiwyg menubar': true,
+	'wysiwyg skin': 'lightgray',
+	'wysiwyg additional buttons': 'searchreplace visualchars,'
+		+ ' charmap ltr rtl pagebreak paste, forecolor backcolor,'
+		+' emoticons media, preview print ',
+	'wysiwyg additional plugins': 'example, table, advlist, anchor,'
+		+ ' autolink, autosave, bbcode, charmap, contextmenu, '
+		+ ' directionality, emoticons, fullpage, hr, media, pagebreak,'
+		+ ' paste, preview, print, searchreplace, textcolor,'
+		+ ' visualblocks, visualchars, wordcount',
+
 });
 
 // Load your project's Models
